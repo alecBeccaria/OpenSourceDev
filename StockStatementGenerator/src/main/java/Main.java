4cdcd92558b37a7250ff663e1c591a9018abc7c6
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-07-15 17:31:40 -0600

    The almost working code

--- StockStatementGenerator/src/main/java/Main.java
+++ StockStatementGenerator/src/main/java/Main.java
@@ -1,7 +1,5 @@
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.commons.math3.util.Precision;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,10 +12,7 @@ public class Main {
         readJSON();
     }
 
-    public static void writeHTML(JSONObject customerRecord, StringBuilder strHTMLTableTradeTop, float cashValue, float stockValue) {
-
-        String cash = (String) customerRecord.get("beginning_balance");
-        String StockValue = (String) customerRecord.get("beginning_balance");
+    public static void writeHTML(JSONObject customerRecord, StringBuilder strHTMLTableTradeTop, double cashValue, double stockValue) {
         String strHTMLTop = "<!DOCTYPE html><html><body><br></br>";
         String strHTMLBottom = "</body></html>";
         String strHTMLCustInfo = String.format("<h1>%s</h1><p>%s %s</p>"
@@ -26,6 +21,7 @@ public class Main {
         + "</p>\r\n", "cash", "stock");
 
         String strHTMLTableTradeBottom = "</table>";
+        String strHTMLAccountBalance = "<br>Starting Balance: " + customerRecord.get("beginning_balance") + "</br>";
         String strFileName = customerRecord.get("account_number").toString();
 
         FileWriter fw;
@@ -36,12 +32,13 @@ public class Main {
             fw.write(strHTMLCustInfo);
             fw.write(strHTMLTableTradeTop.toString());
             fw.write(strHTMLTableTradeBottom);
+            fw.write(strHTMLAccountBalance);
             fw.write(strHTMLSummary);
             fw.write(strHTMLBottom);
             fw.close();
             createPDF(strFileName);
         } catch (Exception e) {
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
     }
 
@@ -72,47 +69,46 @@ public class Main {
 
             Object json = new JSONParser().parse(new FileReader("data\\stocks.json"));
             JSONArray jsonA = (JSONArray) json;
-            long shares;
-            StringBuilder strHTMLTableTradeTop = new StringBuilder("<table style='100%' border='2'>\r\n" +
-                    "<tr>" +
-                    "<th>Type</th>" +
-                    "<th>Symbol</th>" +
-                    "<th>Price</th>" +
-                    "<th>Shares</th>" +
-                    "<th>Total</th></tr>");
-            for(int i = 0; i < jsonA.size(); i++)
-            {
 
+            for(int i = 0; i < 10; i++)
+            {
+                StringBuilder strHTMLTableTradeTop = new StringBuilder("<table style='100%' border='2'>\r\n" +
+                        "<tr>" +
+                        "<th>Type</th>" +
+                        "<th>Symbol</th>" +
+                        "<th>Price</th>" +
+                        "<th>Shares</th>" +
+                        "<th>Total</th></tr>");
                 JSONObject customerRecord = (JSONObject)  jsonA.get(i);
                 String stringCash = (String) customerRecord.get("beginning_balance");
-                float cash = Float.parseFloat(stringCash.substring(1));
-                float stockValue = 0;
+                double cashValue = Double.parseDouble(stringCash.substring(1));
+                double stockValue = 0;
 
                 JSONArray trades = (JSONArray) customerRecord.get("stock_trades");
                 for (Object o : trades) {
                     JSONObject trade = (JSONObject) o;
                     String stockPriceStr = (String) trade.get("price_per_share");
                     float stockPrice = Float.parseFloat(stockPriceStr.substring(1));
-                    shares = (long) trade.get("count_shares");
+                    long shares = (long) trade.get("count_shares");
                     String temp = (String) trade.get("type");
-                    float amountToChange = stockPrice * shares;
+                    double shareTotal = stockPrice * shares;
                     if (temp.equals("Buy")){
-                        cash -= amountToChange;
-                        stockValue += amountToChange;
+                        cashValue -= shareTotal;
+                        stockValue += shareTotal;
                     }
                     else {
-                        cash += amountToChange;
-                        stockValue -= amountToChange;
+                        cashValue = cashValue + shareTotal;
+                        stockValue = stockValue - shareTotal;
                     }
 
                     strHTMLTableTradeTop.append("<tr><td align='center'>" + trade.get("type") + "</td>");
                     strHTMLTableTradeTop.append("<td align='center'>" + trade.get("stock_symbol") +"</td>");
                     strHTMLTableTradeTop.append("<td align='center'>" + trade.get("price_per_share") + "</td>");
                     strHTMLTableTradeTop.append("<td align='center'>" + trade.get("count_shares") + "</td>");
-                    strHTMLTableTradeTop.append("<td align='center'>" + stockPrice * shares + "</td></tr>");
+                    strHTMLTableTradeTop.append("<td align='center'>" + shareTotal + "</td></tr>");
 
                 }
-                writeHTML(customerRecord, strHTMLTableTradeTop, cash, stockValue);
+                writeHTML(customerRecord, strHTMLTableTradeTop, Precision.round(cashValue, 2), Precision.round(stockValue, 2));
             }
 
         }catch (Exception e) {
