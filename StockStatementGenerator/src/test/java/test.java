0ce834aac0de1dde9470cd9904658d4d99b13889
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-07-16 10:02:54 -0600

    Improved PDF Test

--- StockStatementGenerator/src/test/java/test.java
+++ StockStatementGenerator/src/test/java/test.java
@@ -26,23 +26,12 @@ public class test {
 
     @Test
     void testForPDF() {
-        try{
-            Object json = new JSONParser().parse(new FileReader("data\\stocks.json"));
-            for(int i = 0; i < 1; i++){
-                JSONArray jsonA = (JSONArray) json;
-                JSONObject customerRecord = (JSONObject)  jsonA.get(i);
-                String strFileName = customerRecord.get("account_number").toString();
-                File pdf = new File("data\\" + strFileName + ".pdf");
-                StringBuilder strHTMLTableTradeTop = new StringBuilder("test");
-                Main.writeHTML(customerRecord, strHTMLTableTradeTop, 0, 0);
-                boolean test = false;
-                if (pdf.isFile() && !pdf.isDirectory()) {
-                    test = true;
-                }
-                assertTrue(test);
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
+        Main.createPDF("1");
+        File pdf = new File("data\\1.pdf");
+        boolean test = false;
+        if (pdf.isFile() && !pdf.isDirectory()) {
+            test = true;
         }
+        assertTrue(test);
     }
 }
