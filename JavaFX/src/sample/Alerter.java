bb88435a67f5facb73778c97d868b8eaaa56413e
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-08-11 08:52:46 -0600

    Create Alerter.java

---
+++ JavaFX/src/sample/Alerter.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.scene.control.Alert;
+
+import java.awt.*;
+
+public class Alerter {
+    public static void showAlert(Alert.AlertType alertType, Window owner
+    String title, String message) {
+
+    }
+}
